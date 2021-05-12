a72318a3864e0b926980af7745de86e44a996bf6
minhanh0610 <leminhanh0610@gmail.com>
2021-05-12 15:29:47 +0700

    add Entity

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 ## Technologies
 - .Net Core 5.0
 - Entity Framework Core 5.0
+## Install Packages
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.Tools
---
+++ eShopSolution.Data/Entities/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Cart
+    {
+        public int id { get; set; }
+        public int productId { get; set; }
+        public string quatity { get; set; }
+        public decimal price { get; set; }
+        public Guid UserId { get; set; } // guid : 128bit ma dinh danh
+
+        public Product Product { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
