cf69535495b61d7eea5086d7995953cf1caa97f5
minhanh0610 <leminhanh0610@gmail.com>
2021-05-18 15:00:22 +0700

    build application layer

---
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,15 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        public PagedViewModel<ProductViewModel> GetAllByCategory(int CategoryId, int PageIndex, int PageSize);
+    }
+}
