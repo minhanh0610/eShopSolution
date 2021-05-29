50179e415634ebc9bf87f38ebb975ac88d7fe814
minhanh0610 <leminhanh0610@gmail.com>
2021-05-30 01:20:45 +0700

    alert messing using tempdata

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -36,6 +36,10 @@ namespace eShopSolution.AdminApp.Controllers
             };
             var data = await _userApiClient.GetUsersPagings(request);
             ViewBag.KeyWord = keyword;
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMsg = TempData["result"];
+            }
             return View(data.ResultObj);
         }
 
@@ -60,7 +64,11 @@ namespace eShopSolution.AdminApp.Controllers
 
             var result = await _userApiClient.RegisterUser(request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = " add successful new users";
                 return RedirectToAction("Index");
+            }
+                
             ModelState.AddModelError("", result.Message);
             return View(request);
         }
@@ -94,7 +102,11 @@ namespace eShopSolution.AdminApp.Controllers
 
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Cập nhật người dùng thành công";
                 return RedirectToAction("Index");
+            }
+                
 
             ModelState.AddModelError("", result.Message);
             return View(request);
@@ -125,7 +137,11 @@ namespace eShopSolution.AdminApp.Controllers
 
             var result = await _userApiClient.Delete(request.Id);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Xóa người dùng thành công";
                 return RedirectToAction("Index");
+            }
+               
 
             ModelState.AddModelError("", result.Message);
             return View(request);
