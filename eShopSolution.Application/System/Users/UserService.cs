062edfb17e22f72fc59edc4c2a63b53fc5c72399
minhanh0610 <leminhanh0610@gmail.com>
2021-05-29 21:54:55 +0700

    delete user

--- eShopSolution.Application/System/Users/UserService.cs
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -63,6 +63,20 @@ namespace eShopSolution.Application.System.Users
             return new ApiSuccessResult<string>(new JwtSecurityTokenHandler().WriteToken(token));
         }
 
+        public async Task<ApiResult<bool>> Delete(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+            if (user == null)
+            {
+                return new ApiErrorResult<bool>("User doesn't exist");
+            }
+            var reult = await _userManager.DeleteAsync(user);
+            if (reult.Succeeded)
+                return new ApiSuccessResult<bool>();
+
+            return new ApiErrorResult<bool>("Deleting don't success");
+        }
+
         public async Task<ApiResult<UserVm>> GetById(Guid id)
         {
             var user = await _userManager.FindByIdAsync(id.ToString());
