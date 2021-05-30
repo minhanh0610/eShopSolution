3e98f3d6ae2d6bf5d830b62e6c1e8a8a16a820fd
minhanh0610 <leminhanh0610@gmail.com>
2021-05-30 17:21:16 +0700

    role assign

--- eShopSolution.AdminApp/Services/IUserApiClient.cs
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -21,5 +21,7 @@ namespace eShopSolution.AdminApp.Services
         Task<ApiResult<UserVm>> GetById(Guid id);
 
         Task<ApiResult<bool>> Delete(Guid id);
+
+        Task<ApiResult<bool>> RoleAssign(Guid id, RoleAssignRequest request);
     }
 }
--- eShopSolution.Application/System/Users/IUserService.cs
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -19,5 +19,7 @@ namespace eShopSolution.Application.System.Users
 
         Task<ApiResult<UserVm>> GetById(Guid id);
         Task<ApiResult<bool>> Delete(Guid id);
+
+        Task<ApiResult<bool>> RoleAssign(Guid id, RoleAssignRequest request);
     }
 }
