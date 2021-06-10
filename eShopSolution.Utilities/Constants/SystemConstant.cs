e587c28dff5e6000d84d2faba7612667dfdac9b1
minhanh0610 <leminhanh0610@gmail.com>
2021-06-10 21:31:50 +0700

    list cart items

--- eShopSolution.Utilities/Constants/SystemConstant.cs
+++ eShopSolution.Utilities/Constants/SystemConstant.cs
@@ -9,9 +9,10 @@ namespace eShopSolution.Utilities.Constants
        public class SystemConstants
         {
             public const string MainConnectionString = "eShopSolutionDb";
+            public const string CartSession = "CartSession";
 
-       
-            public class AppSettings
+
+        public class AppSettings
             {
                 public const string DefaultLanguageId = "DefaultLanguageId";
                 public const string Token = "Token";
